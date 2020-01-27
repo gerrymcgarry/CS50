81e232b5d1a0e845406f1a84528cb1501953461c
Gerry McGarry <gerrymcgarry@gmail.com>
2020-01-27 21:16:33 +0000

    started Speller

---
+++ pset5/speller/dictionary.c
@@ -0,0 +1,83 @@
+// Implements a dictionary's functionality
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dictionary.h"
+#include <string.h>
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+
+node *table[N];
+
+char dict_word[LENGTH+1];
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO
+    return 0;
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    // TODO
+    // Open dictionary file
+    FILE *inptr = fopen(dictionary, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", dictionary);
+        return 1;
+    }
+    // Read strings from file one at a time
+    while(fscanf(inptr, "%s", dict_word) != EOF)
+    {
+        // Create a new node for each word
+        node *new_node = malloc(sizeof(node));
+        strcpy(new_node->word, dict_word);
+        printf("%s\n", new_node->word );
+
+        // Hash word to obtain hash value
+        int index = hash(dict_word);
+        // Insert node into hash table at that location
+        new_node->next = table[index]->next->next;
+        table[index]->next = new_node;
+
+    }
+
+
+
+    return false;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
